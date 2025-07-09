26495a2f942fec8e279b95643fa9510fff8dcb8e
Cengiz <cengiz@puzly.co>
2025-07-09 19:11:17 +0600

    Rearranged config file.

--- android/config.gradle.kts
+++ android/config.gradle.kts
@@ -9,15 +9,13 @@ extra.apply {
 	set("pluginPackageName", "org.godotengine.plugin.android.admob")
 	set("pluginVersion", "5.0")
 
-	// Godot version
+	// Godot
 	set("godotVersion", "4.5")
 	set("releaseType", "beta3")
-
-	// Directory paths (relative to module)
-	set("demoAddOnsDirectory", "../../demo/addons")
-	set("templateDirectory", "../../addon")
-
-	// Derived variables
 	set("godotAarUrl", "https://github.com/godotengine/godot-builds/releases/download/${get("godotVersion")}-${get("releaseType")}/godot-lib.${get("godotVersion")}.${get("releaseType")}.template_release.aar")
 	set("godotAarFile", "godot-lib-${get("godotVersion")}.${get("releaseType")}.aar")
+
+	// Demo
+	set("demoAddOnsDirectory", "../../demo/addons")
+	set("templateDirectory", "../../addon")
 }
